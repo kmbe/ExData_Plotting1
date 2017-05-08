67bb2a69cd6770e32c9cab6957c631e8ec059079
kmbe <kennedy890@hotmail.com>
2017-05-08 19:51:50 +0100

    Update plot1.R

--- plot1.R
+++ plot1.R
@@ -5,7 +5,7 @@ ehc <- read.table(filename, sep = ";", dec = ".", header = TRUE, stringsAsFactor
 sbset <- ehc[ehc$Date %in% c("1/2/2007", "2/2/2007"),]
 
 #str(sbset)
-
+#Converting data type from Char to int to ready variable for plotting.
 GlobalActivePower <- as.numeric(sbset$Global_active_power)
 
 png("Plot1.png", width = 480, height = 480)
